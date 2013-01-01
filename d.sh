71ed64f2c807d48edde5159a620409c01ea76dfd
Shawn McKinney <smckinn@smckinnlt-lr02.(none)>
2013-01-01 15:53:34 -0600

    RC21

--- d.sh
+++ d.sh
@@ -2,4 +2,4 @@
 export JAVA_HOME=/opt/jdk1.7.0_10
 export ANT_HOME=./apache-ant
 export M2_HOME=/usr/share/maven2
-mvn deploy -DperformRelease=true -Dgpg.passphrase=PASSPHRASE
+mvn deploy -DperformRelease=true -Dgpg.passphrase=$1
