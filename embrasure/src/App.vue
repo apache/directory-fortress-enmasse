f42757916c94a4e5a090c0b5f5672ea000bdf105
Kiran Ayyagari <kayyagari@apache.org>
2018-11-20 21:29:08 +0530

    highlight the correct menu option based on the path

--- embrasure/src/App.vue
+++ embrasure/src/App.vue
@@ -43,6 +43,12 @@ export default {
     return {
       activeIndex: '/'
     }
+  },
+  created() {
+    let path = this.$router.history.current.path
+    if(path != '/') {
+      this.activeIndex = path
+    }
   }
 }
 </script>
